6d2bb6a873ef283207e870a26df16233b711f0f2
Jonathan-iths <jonathananders.vidaltoll@iths.se>
2025-10-05 17:29:50 +0200

    ggg

--- cha3/main.java
+++ cha3/main.java
@@ -3,6 +3,7 @@ package cha3;
 public class main {
     public static void main(String[] args) {
         
+        tid.toString12(13,10,50);
     }
     
 }
--- cha3/tid.java
+++ cha3/tid.java
@@ -5,33 +5,63 @@ public class tid {
     int m;
     int s;
     
-    if (h<0 || h>24 || m<0 || m>59 || s<0 || s>59)
-            throw new IllegalArgumentException("Time: Bad value:" + h + ":" + m + ":" + s);
+    int getH(){
+        return this.h;
+    }
+    int getM(){
+        return this.m;
+    }
+    int getS(){
+        return this.s;
+    }
+    void setH(int h){
+        this.h = h;
+    }
+    void setM(int m){
+        this.m = m;
+    }
+    void setS(int s){
+        this.s =s;
+    }
 
+    static void toString1(int h,int m,int s){
+        System.out.print(m<10? h + ":" + "0" + m +":" + s: h + ":" + m +":" + s);
+    }
+
+    static void toString12(int h, int m, int s){
+        String x = "AM";
+        if(h>12){
+            h = h-12; x = "PM";
+        }
+        if(h==12){
+            x = "PM";
+        } 
+        toString1(h,m,s);
+        System.out.print(" " + x);
+    }
+
+    tid(){
+    }
 
     tid(int h){
-        this.h = h;
-
-            
+        this.h = h;       
         }
-
-    }
 
     tid(int h, int m){
         this.h =h;
         this.m =m;
-
-
-
     }
 
     tid(int h, int m, int s){
+        if (h<0 || h>23 || m<0 || m>59 || s<0 || s>59){
+            throw new IllegalArgumentException("Time: Bad value:" + h + ":" + m + ":" + s);
+    }
         this.s=s;
         this.h =h;
         this.m =m;
-
-
-
     }
-    
+
 }
+
+    
+
